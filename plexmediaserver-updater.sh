fef8782ede1c30dc473d0f6479eef98def51a076
poneli <elisson.pontus@gmail.com>
2021-10-05 13:35:50 +0200

    Fix log to file when run in cron

--- plexmediaserver-updater.sh
+++ plexmediaserver-updater.sh
@@ -10,30 +10,30 @@ currentversion=$(dpkg -s plexmediaserver | awk -F'[- ]' '/^Version:/ { print $2 
 plextoken="PLEXTOKEN-HERE" # Find your plex token -> (https://support.plex.tv/articles/204059436-finding-an-authentication-token-x-plex-token/)
 downloadfolder="/change/me/example/directory" # No trailing slash
 #### </VARIABLES>
-if [[ $EUID > 0 ]]; then
+if [[ $EUID -gt 0 ]]; then
 	printf "Run with sudo... \n"
 	exit
 fi
 
 if [[ $latestversion > $currentversion ]]; then
-	printf "Downloading plexmediaserver to %s... \n" $downloadfolder
+	printf "Downloading plexmediaserver to %s... \n" "$downloadfolder"
 	wget -q -O plexmediaserver-$latestversion.deb "https://plex.tv/downloads/latest/5?channel=8&build=linux-x86_64&distro=debian&X-Plex-Token=$plextoken" 2>&1 >/dev/null
 	printf "Stopping plexmediaserver... \n"
 	systemctl stop plexmediaserver.service 2>&1 >/dev/null
 	printf "Installing update... \n"
 	dpkg -i plexmediaserver-$latestversion.deb 2>&1 >/dev/null
 	if [[ $(dpkg -s plexmediaserver | awk -F'[- ]' '/^Version:/ { print $2 }') = $latestversion ]]; then
-	  printf "plexmediaserver updated successfully from version %s to %s... \n" $currentversion $latestversion
-	  printf "%(%Y-%m-%d %H:%M:%S)T [SUCCESS] plexmediaserver updated to %s... \n" $(date +%s) $latestversion | tee -a update.log >/dev/null
+	  printf "plexmediaserver updated successfully from version %s to %s... \n" "$currentversion" "$latestversion"
+	  printf -- "%(%Y-%m-%d %H:%M:%S)T [SUCCESS] plexmediaserver updated to %s... \n" "$(date +%s)" "$latestversion" | tee -a $downloadfolder/update.log >/dev/null
 	  printf "Starting plexmediaserver... \n"
 	  systemctl start plexmediaserver.service 2>&1 >/dev/null
-	  printf "Cleaning up %s... \n" $downloadfolder
+	  printf "Cleaning up %s... \n" "$downloadfolder"
 	  rm -f $downloadfolder/*.deb
 	else
-	  printf "Installation of plexmediaserver %s failed... \nTerminated... \n" $latestversion
-	  printf "%(%Y-%m-%d %H:%M:%S)T [ERROR] plexmediaserver %s update failed... \n" $(date +%s) $latestversion | tee -a update.log >/dev/null
+	  printf "Installation of plexmediaserver %s failed... \nTerminated... \n" "$latestversion"
+	  printf -- "%(%Y-%m-%d %H:%M:%S)T [ERROR] plexmediaserver %s update failed... \n" "$(date +%s)" "$latestversion" | tee -a $downloadfolder/update.log >/dev/null
 	fi
 else
-	printf "plexmediaserver %s is already installed... \nTerminated... \n" $latestversion
-	printf "%(%Y-%m-%d %H:%M:%S)T [INFO] plexmediaserver %s is already installed... \n" $(date +%s) $latestversion | tee -a update.log >/dev/null
+	printf "plexmediaserver %s is already installed... \nTerminated... \n" "$latestversion"
+	printf -- "%(%Y-%m-%d %H:%M:%S)T [INFO] plexmediaserver %s is already installed... \n" "$(date +%s)" "$latestversion" | tee -a $downloadfolder/update.log >/dev/null
 fi
